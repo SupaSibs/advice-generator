88ccef7255fe0e6d4111c0bdc641cfbdcba9a11b
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-06-04 06:12:24 +0000

    finally fixed the generator

--- src/lib/Advice.svelte
+++ src/lib/Advice.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
- import Advice from "./advicegen.ts"
-  let advice;
+  let advice; let fetcher; let res;
  let adv; let id;
 async function generateAdvice() {
 
-  
- advice = Advice()
-  setTimeout(5000, () => null)
+fetcher = await fetch("https://api.adviceslip.com/advice")
+  advice = await fetcher.json()
   adv = advice.slip.advice
-  id = advice.slip.id;
+  id = advice.slip.id
 }
   generateAdvice()
   console.log(advice)
