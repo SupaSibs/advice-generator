941c339c291d012a96609f61e8906b986ff11aa5
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-06-06 04:48:03 +0000

    6

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,17 @@
   import AdviceGen from './lib/Advice.svelte'
 	
 </script>
+<div>
 <AdviceGen />
+</div>
   <Attribution />
-    <style>
-        @use "./lib/vars as *";
+    <style lang="scss">
+   @use "./lib/vars" as *;
+div {
+   background-color: $DarkBlue;
+    min-height: 100vh;
+       display: flex;
+  justify-content: center;
+  align-items: center;
+  } 
     </style>
--- src/lib/Advice.svelte
+++ src/lib/Advice.svelte
@@ -13,19 +13,40 @@ fetcher = await fetch("https://api.adviceslip.com/advice")
 
 </script>
 <div>
- <p>Advice #{id}</p>
-  <p>"{adv}"</p>
+ <p id="id">Advice #{id}</p>
+  <p id="adv">"{adv}"</p>
+  <button on:click={generateAdvice}>
+New Advice
+  </button>
 </div>
 <style lang="scss">
   @use "./vars" as *;
 div {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  min-height: 40%;
+  min-height: 50%;
+  max-height: 50%;
   background-color: $DarkGrayishBlue;
   color: white;
+  max-width: 50%;
+  #adv {
+  font-size: $FontQuote;
+  color:whiten;
+  text-align: center;
+  }
+  #id {
+  color: $NeonGreen;
+  letter-spacing: 3px;
+  }
 }
+  @media screen and (max-width: 700) {
+  div {
+  min-height: 70%;
+  max-height: 70%
+  }
+  }
 </style>
   
   
